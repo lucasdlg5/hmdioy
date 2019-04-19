4b2cdbd4ef311ea3356e380f8d8d21097197715e
Lucas Domingos Leão Gomes <lucasdomingosleao55@hotmail.com>
2019-04-19 00:49:00 -0300

    Adicionando nova tela - Dashboard

--- hmdioy/src/app/app.module.ts
+++ hmdioy/src/app/app.module.ts
@@ -9,11 +9,13 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
