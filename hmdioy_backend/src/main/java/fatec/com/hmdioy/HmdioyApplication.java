5b3b47286b5d3d270b96e77374d378f882dbdfc7
Lucas Domingos Leão Gomes <lgomes@dannemann.com.br>
2019-05-10 20:45:52 -0300

    MVN rodando sem erros

--- hmdioy_backend/src/main/java/fatec/com/hmdioy/HmdioyApplication.java
+++ hmdioy_backend/src/main/java/fatec/com/hmdioy/HmdioyApplication.java
@@ -1,5 +1,5 @@
 package fatec.com.hmdioy;
-
+//mvn clean install spring-boot:run
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
