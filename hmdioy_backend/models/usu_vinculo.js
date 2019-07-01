2f30a82a2215f3afaac7d8237698d0e566f7fa9c
Lucas Domingos Leão Gomes <lucasdomingosleao55@hotmail.com>
2019-07-01 13:44:10 -0300

    Adicionando estrura nao testada usu_vinculo

--- hmdioy_backend/models/usu_vinculo.js
+++ hmdioy_backend/models/usu_vinculo.js
@@ -1,14 +1,15 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const usu_vinculo = sequelize.define('usu_vinculo', {
-    vin_id: DataTypes.INTEGER,
+    vin_id: {type: DataTypes.INTEGER, autoIncrement: true, unique: true, primaryKey: true, comment: "Campo de ID da tabela" },
     vin_usu_origem: DataTypes.INTEGER,
     vin_usu_destino: DataTypes.INTEGER,
     vin_aceito: DataTypes.BOOLEAN,
     vin_alerta: DataTypes.BOOLEAN
   }, {});
   usu_vinculo.associate = function(models) {
-    // associations can be defined here
+    usu_vinculo.hasOne(models.usuario, {as: 'vin_usu_origem', foreignKey: 'usu_id'})
+    usu_vinculo.hasOne(models.usuario, {as: 'vin_usu_destino', foreignKey: 'usu_id'})
   };
   return usu_vinculo;
 };
