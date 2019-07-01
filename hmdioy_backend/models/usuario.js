6d5fb5b088fc8f73811c661db9b753ad4cb2a8e2
Lucas Domingos Leão Gomes <lucasdomingosleao55@hotmail.com>
2019-07-01 13:35:45 -0300

    Estrutura basica nao testada de Usuario Model

--- hmdioy_backend/models/usuario.js
+++ hmdioy_backend/models/usuario.js
@@ -1,15 +1,15 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const usuario = sequelize.define('usuario', {
-    usu_id: DataTypes.INTEGER,
-    usu_nome: DataTypes.STRING,
-    usu_email: DataTypes.STRING,
-    usu_username: DataTypes.STRING,
-    usu_whatsapp: DataTypes.STRING,
-    usu_foto: DataTypes.BLOB
+    usu_id: {type: DataTypes.INTEGER, autoIncrement: true, unique: true, primaryKey: true, comment: "Campo de ID da tabela" },
+    usu_nome: DataTypes.STRING(70),
+    usu_email: DataTypes.STRING(100),
+    usu_username: DataTypes.STRING(30),
+    usu_whatsapp: DataTypes.STRING(17),
+    usu_foto: DataTypes.BLOB('long') 
   }, {});
   usuario.associate = function(models) {
-    // associations can be defined here
+    usuario.belongsTo(models.usu_vinculo);
   };
   return usuario;
 };
